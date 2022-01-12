3ae9a0abdd1837918c76f333776b024257977618
İlayda Atmaca <atmacailyada@gmail.com>
2022-01-13 00:10:17 +0300

    laser defender

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 2f;
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+        GameSession myGame = FindObjectOfType<GameSession>();
+        if (myGame != null)
+        {
+            myGame.ResetGame();
+        }
+
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene("Start Menu 1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Enemy Wave Configuration")]
+
+public class WaveConfig : ScriptableObject
+{
+
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab(){    return enemyPrefab; }
+    public List<Transform> GetWayPoints() 
+    {
+        var waveWaypoints = new List<Transform>();
+
+        foreach(Transform child in pathPrefab.transform )
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints; 
+    }
+    public float GettimeBetweenSpawns() { return timeBetweenSpawns; }
+    public float GetspawnRandomFactor() { return spawnRandomFactor; }
+    public int GetnumberOfEnemies() { return numberOfEnemies; }
+    public float GetmoveSpeed() { return moveSpeed; }
+
+
+
+}
